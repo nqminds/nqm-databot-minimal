2e6e98c5e32f23030e3321ccf390d902fed01b7c
toby ealden <toby.ealden@gmail.com>
2016-09-20 20:00:20 +0100

    npm init

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-// Load the nqm input module for receiving input from the process host.
-var input = require("nqm-databot-utils").input;
 
-// Load the nqm output module for communicating progress and info back to the process host.
-var output = require("nqm-databot-utils").output;
-
-// debug output is sent to the TDX when the databot completes. 
-output.debug("starting");
-
-// Read any data passed from the process host. Specify we're expecting JSON data.
-input.read("json", function(err, inputArgs) {
-  if (err) {
-    // Errors are transmitted to the TDX.
-    output.error("failed to read input: %s", err.message);
-    // Exit code non-zero => tells the TDX this databot failed.
-    process.exit(1);
-  }
-
+function databot(input, output, context) {
   // PROGRESS output is mandatory, to inform the host of progress.
   output.progress(0);
 
@@ -36,4 +20,10 @@ input.read("json", function(err, inputArgs) {
       process.exit(0);
     }
   }, 2500);  
-});
+}
+
+// Load the nqm input module for receiving input from the process host.
+var input = require("nqm-databot-utils").input;
+
+// Read any data passed from the process host. Specify we're expecting JSON data.
+input.pipe(databot);
